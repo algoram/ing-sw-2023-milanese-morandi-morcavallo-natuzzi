dc5080aa47bac0c550349f8e68c53a581bcba45d
Ernesto Natuzzi <ernesto.natuzzi@mail.polimi.it>
2023-03-28 11:37:36 +0200

    Declaration of common goal class

---
+++ src/main/java/myshelfie_model/goal/common_goal/FourGroups4Tiles.java
@@ -0,0 +1,13 @@
+package myshelfie_model.goal.common_goal;
+
+import myshelfie_model.Tile;
+import myshelfie_model.Type;
+import myshelfie_model.player.Bookshelf;
+
+public class FourGroup4Tiles extends CommonGoal {
+
+    public FourLinesMax3(int numberOfPlayers) {
+        super(numberOfPlayers);
+    }
+
+}
