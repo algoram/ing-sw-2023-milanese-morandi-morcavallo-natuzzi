f4a098592b2121f458feeed916573ba6ba515e00
Enrico Milanese <enricomilanese913@gmail.com>
2023-03-28 14:09:43 +0200

    Cleaned up a few thing

--- src/main/java/myshelfie_model/player/Player.java
+++ src/main/java/myshelfie_model/player/Player.java
@@ -9,7 +9,21 @@ public class Player {
     private PersonalGoal personalGoal;
 
     public int getPersonalGoalPoints(){
-        
+        // TODO: implement method
+
+        return 0;
+    }
+
+    public int getCommonGoalPoints(int commonGoalIndex) {
+        return commonGoalsPoints[commonGoalIndex];
+    }
+
+    public void setCommonGoalPoints(int commonGoalIndex, int points) {
+        if (commonGoalsPoints[commonGoalIndex] == -1) {
+            return;
+        }
+
+        commonGoalsPoints[commonGoalIndex] = points;
     }
 
     public Bookshelf getBookshelf() {
