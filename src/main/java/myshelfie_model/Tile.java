6a718a0ffb5450cc3df7523e4ac07f32abcf8398
Ernesto Natuzzi <ernesto.natuzzi@mail.polimi.com>
2023-04-05 15:52:26 +0200

    remove of BoardPosition.java, fixed bugs in Tile

--- src/main/java/myshelfie_model/Tile.java
+++ src/main/java/myshelfie_model/Tile.java
@@ -2,19 +2,11 @@ package myshelfie_model;
 
 public class Tile {
     private final Type type;
-    private BoardPosition position;
-
 
     public Tile(Type t){
         this.type = t;
-        this.position = new BoardPosition(-1,-1);
     }
     public Type getType(){
         return this.type;
-    };
-    public BoardPosition getPosition(){ return this.position;};
-    public void setPosition(int row,int col){
-        this.position.setPosition(row,col);
     }
-
 }
