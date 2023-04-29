669f6a7cc9449bfd15bab0ae8185535e9baee620
Ernesto Natuzzi <ernesto.natuzzi@mail.polimi.it>
2023-04-29 12:48:56 +0200

    Settings

--- src/main/java/myshelfie_view/View.java
+++ src/main/java/myshelfie_view/View.java
@@ -1,13 +1,23 @@
 package myshelfie_view;
 
 import myshelfie_controller.EventDispatcher;
+import myshelfie_controller.Settings;
+import myshelfie_view.cli.CliView;
+import myshelfie_view.gui.GuiView;
 
-public class View {
+public abstract class View {
 
-    protected EventDispatcher eventDispatcher;
-
-    public View(EventDispatcher dispatcher) {
-        eventDispatcher = dispatcher;
+    public static View getInstance() {
+        if (Settings.getInstance().getViewType().equals("cli")) {
+            return CliView.getInstanceCli();
+        } else {
+            return GuiView.getInstance();
+        }
     }
+
+    //TODO no exceptions
+    public abstract void init() throws Exception;
+
+
 
 }
